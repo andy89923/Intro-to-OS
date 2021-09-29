1e1650f78dc7be8498305d33f8e15dc1d4ebbd24
tfchen0923 <tfchen0923@bsd3.cs.nctu.edu.tw>
2021-09-30 01:48:08 +0800

    HW1 upload

--- HW1/hw1_3.cpp
+++ HW1/hw1_3.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <cstring>
+#include <string>
 using namespace std;
-#define MAX_LINE 80
+typedef pid_t PID;
+#define MAXARG 80
 
 int main() {
-	char *arg[MAX_LINE/2+1];
-	int keep_run = 1;
-	
-	while (keep_run) {
+    char* arg[MAXARG];
+
+    string cmd;
+
+	while (true) {
 		cout << "OSH>";
-		fflush(stdout);
-		
-				
-		keep_run = 0;
-		if (!keep_run) break;
+        fflush(stdout);
+        
+        int arg_cnt = 1;
+        for (int i = 0; i < MAXARG; i++) arg[i] = NULL;
+
+        getline(cin, cmd);
+        int n = (int) cmd.length(), k;
+
+        string pro_nam = "";
+        for (int i = 0; i < n; i++) {
+            if (cmd[i] == ' ' or cmd[i] == '\n') break;
+            
+            pro_nam += cmd[i];
+            k = i;
+        }
+        arg[0] = strdup(pro_nam.c_str());
+
+        string now = "";
+        for (int i = k + 2; i < n; i++) {
+            if (cmd[i] == ' ' or cmd[i] == '\n') {
+                if (now == "") continue;
+
+                arg[arg_cnt] = strdup(now.c_str());
+                arg_cnt += 1;
+                
+                continue;
+            }
+            now += cmd[i];
+        }
+        if (now != "") {
+            arg[arg_cnt] = strdup(now.c_str());
+            arg_cnt += 1;
+        }
+	    if (pro_nam == "exit") break;
+
+        PID pid = fork();
+        if (pid == 0) {
+            execvp(pro_nam.c_str(), arg);
+            return 0;
+        }
+        wait(NULL);
 	}
 	return 0;
 }
